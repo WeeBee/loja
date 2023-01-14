e7f757a7b20487ca938bc74966b01ba9b2b32fde
WeeBee <wilsonjamm@gmail.com>
2023-01-14 10:24:15 -0300

    pausa na aula 05.04: Consultas com JPQL

--- src/main/java/br/com/alura/loja/dao/ProdutoDao.java
+++ src/main/java/br/com/alura/loja/dao/ProdutoDao.java
@@ -37,4 +37,14 @@ public class ProdutoDao {
 		String jpql = "SELECT p FROM Produto p";
 		return em.createQuery(jpql, Produto.class).getResultList();
 	}
+	
+	public List<Produto> buscarPorNome(String nome){
+		String jpql = "SELECT p FROM Produto p WHERE p.nome = :nome"; //WHERE p.nome = ?1
+		return em.createQuery(jpql, Produto.class).setParameter("nome", nome).getResultList();
+	}
+	
+	public List<Produto> buscarPorNomeDaCategoria(String nome){
+		String jpql = "SELECT p FROM Produto p WHERE p.categoria.nome = :nome"; //WHERE p.nome = ?1
+		return em.createQuery(jpql, Produto.class).setParameter("nome", nome).getResultList();
+	}
 }
