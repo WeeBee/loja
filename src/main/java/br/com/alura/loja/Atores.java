76405f9dcf3ee0ed80a43c6d397de329152e431f
WeeBee <wilsonjamm@gmail.com>
2023-01-25 14:27:09 -0300

    finalizado

--- src/main/java/br/com/alura/loja/Atores.java
+++ src/main/java/br/com/alura/loja/Atores.java
@@ -1,9 +1,6 @@
 package br.com.alura.loja;
-
 import java.util.List;
-
 import javax.persistence.EntityManager;
-
 import br.com.alura.loja.dao.AtorDao;
 import br.com.alura.loja.modelo.Ator;
 import br.com.alura.loja.util.JPAUtil;
@@ -16,13 +13,13 @@ public class Atores {
 		List<Ator> listaAtores = atorDao.listaAtores();
 		listaAtores.forEach(a -> System.out.println(a.getPrimeiroNome() + " " + a.getSobrenome()));
 		System.out.println("quantidade de registros: " + listaAtores.size());
-		
+
 		System.out.println("\npesquisando somente os Williams, temos:\n");
 		List<Ator> listaWilliams = atorDao.buscarPorParametros(null, "Williams");
 		listaWilliams.forEach(w -> System.out.println(w.getPrimeiroNome() + " " + w.getSobrenome()));
-		
-		System.out.println("\npesquisando os Williams com Criteria:\n");
-		List<Ator> listaWilliamsComCriteria = atorDao.buscaPorParametrosComCriteria(null, "Williams");
-		listaWilliamsComCriteria.forEach(w2 -> System.out.println(w2.getPrimeiroNome() + " " + w2.getSobrenome()));
+
+		System.out.println("\npesquisando os Kenneth com Criteria:\n");
+		List<Ator> listaKennethComCriteria = atorDao.buscaPorParametrosComCriteria("Kenneth", null);
+		listaKennethComCriteria.forEach(w2 -> System.out.println(w2.getPrimeiroNome() + " " + w2.getSobrenome()));
 	}
 }
